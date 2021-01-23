f63ebf99dd558773b1ce3bb6c12915cb8634b3d6
amueller-tech <57542733+amueller-tech@users.noreply.github.com>
2021-01-23 22:42:38 +0100

    cut down scope for authentication

--- auth.js
+++ auth.js
@@ -16,27 +16,7 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 
-const scopes = [
-  'ugc-image-upload',
-  'user-read-playback-state',
-  'user-modify-playback-state',
-  'user-read-currently-playing',
-  'streaming',
-  'app-remote-control',
-  'user-read-email',
-  'user-read-private',
-  'playlist-read-collaborative',
-  'playlist-modify-public',
-  'playlist-read-private',
-  'playlist-modify-private',
-  'user-library-modify',
-  'user-library-read',
-  'user-top-read',
-  'user-read-playback-position',
-  'user-read-recently-played',
-  'user-follow-read',
-  'user-follow-modify'
-];
+let scopes = ['streaming', 'user-read-currently-playing', 'user-modify-playback-state', 'user-read-playback-state'];
 
 const spotifyApi = new SpotifyWebApi({
   redirectUri: 'http://localhost:8888/callback',
